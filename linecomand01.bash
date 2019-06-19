35edd07bffb651fad7cc7ef02b6f25898a097810
SandroAnjos <31393134+SandroAnjos@users.noreply.github.com>
2019-06-19 07:35:17 -0300

    Create linecomand01.bash

---
+++ linecomand01.bash
@@ -0,0 +1,32 @@
+#################################################
+#Verificar quantidade de processadores
+<cpuinfo>
+sudo cat /proc/cpuinfo
+</cpuinfo>
+#################################################
+#Quantidade de memória
+<mainfo>
+sudo cat /proc/mainfo
+</mainfo>
+#################################################
+#Quantidade de memoria disponivel
+<free>
+sudo free -m
+</free>
+#################################################
+#Processos em aberto
+<ps aux>
+sudo ps aux
+</ps aux>
+#################################################
+#
+<top>
+sudo top
+</top>
+##################################################
+#
+<uptime>
+sudo uptime
+</uptime>
+##################################################	
+#
